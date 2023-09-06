6d339ae2996c9cf3a3ddfe52032958ef5d9f2bf0
Alexy Mantha <alexy@mantha.dev>
2023-09-06 00:07:25 -0400

    add progressive sync status

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -3,13 +3,20 @@ import * as React from 'react';
 export const Extension = (props: {
   tree: any;
   resource: any;
-}) => (
-  <div>Hello {props.resource.metadata.name}!</div>
-);
+}) => {
+  console.log()
+  return (
+    <div className='application-status-panel__item-value' style={{ margin: 'auto 0' }}>
+      <a className='neutral'>
+        <i className={`fa fa-pause-circle`} /> Progressive Sync
+      </a>
+    </div>
+  )
+}
 
 export const component = Extension;
 
 // Register the component extension in ArgoCD
 ((window: any) => {
-  window?.extensionsAPI?.registerStatusBarExtension(component, 'Progressive Sync', 'fa-hourglass');
+  window?.extensionsAPI?.registerStatusBarExtension(component, 'PROGRESSIVE SYNC');
 })(window)
